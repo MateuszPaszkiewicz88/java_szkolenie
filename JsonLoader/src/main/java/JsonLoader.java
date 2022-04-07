6a27cc158df3dd9a63310308261b994d56675ec6
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-07 13:20:50 +0200

    rebuild maine to properly use sheet as in json

--- JsonLoader/src/main/java/JsonLoader.java
+++ JsonLoader/src/main/java/JsonLoader.java
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import json.Sample;
+import json.Sheet;
 import org.apache.log4j.Logger;
 
 import java.awt.image.SampleModel;
@@ -20,15 +21,15 @@ public class JsonLoader {
 
     final static Logger logger = Logger.getLogger(JsonLoader.class);
 
-    public static <Lis> void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         Gson gson = new Gson();
-        Reader reader = new FileReader("/home/mateusz/IdeaProjects/JsonLoader/src/main/resources/json.json");
+        Reader reader = new FileReader("/home/mateuszpaszkiewicz/Java/java_szkolenie/JsonLoader/src/main/resources/json.json");
 
         JsonElement elements = gson.fromJson(reader,JsonElement.class);
+        Sheet sheet = gson.fromJson(elements, Sheet.class);
 
-        Sample[] samples = gson.fromJson(elements, Sample[].class);
-        Stream.of(samples).map(Sample::toString).forEach(logger::info);
+        System.out.println(sheet);
     }
 
 }
