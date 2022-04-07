db4a9249fefcf6db1d922a981af5490bc28a3284
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-07 13:21:33 +0200

    add serialized name @

--- JsonLoader/src/main/java/json/Sample.java
+++ JsonLoader/src/main/java/json/Sample.java
@@ -1,17 +1,39 @@
 package json;
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
 @Setter
 @ToString
 
-public class Sample {
-    private Integer serialNumber;
+public class Sample{
+
+    @SerializedName("Serial Number")
+    private BigDecimal serialNumber;
+
+    @SerializedName("Company Name")
     private String companyName;
+
+    @SerializedName("Employee Markme")
     private String employeeMark;
+
+    @SerializedName("Description")
     private String description;
+
+    @SerializedName("Leave")
     private Integer leave;
+
+    public Sample(BigDecimal serialNumber, String companyName, String employeeMark, String description, Integer leave) {
+        this.serialNumber = serialNumber;
+        this.companyName = companyName;
+        this.employeeMark = employeeMark;
+        this.description = description;
+        this.leave = leave;
+    }
+
 }
