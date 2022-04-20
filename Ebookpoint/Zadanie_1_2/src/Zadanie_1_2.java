6b331e6b628998d3573810d884567e153f97885c
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 10:50:24 +0200

    format result in java

---
+++ Ebookpoint/Zadanie_1_2/src/Zadanie_1_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Zadanie_1_2 {
+    public static void main(String[] args) {
+        System.out.println("Podaj z jaką dokładnością ma być wyświetlona liczba Pi.");
+        System.out.printf("Pi = " + "%11.10f", Math.PI);
+
+    }
+}
+
+
