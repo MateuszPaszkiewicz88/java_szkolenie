55ac3bb24c5dcde707f67a4adce0421e3b4003bc
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 11:18:27 +0200

    divide without rest

---
+++ Ebookpoint/Zadanie_1_5/src/Zad_1_5.java
@@ -0,0 +1,8 @@
+public class Zad_1_5 {
+    public static void main(String[] args) {
+        int a = 37;
+        int b = 11;
+
+        System.out.println("Liczba a podzielona przez b = "+a/b);
+    }
+}
