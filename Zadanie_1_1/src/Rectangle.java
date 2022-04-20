278ac81951fcb2bd0ed2cc36bf1b959a3cf3b30f
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 09:09:10 +0200

    add Scanner

---
+++ Zadanie_1_1/src/Rectangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    double a,b, field;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
