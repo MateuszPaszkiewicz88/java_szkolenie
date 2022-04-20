f6386b51321396ba7251bab5a29143f35a2374af
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 10:26:26 +0200

    move to folder

--- Ebookpoint/Zadanie_1_1/src/Rectangle.java
+++ Ebookpoint/Zadanie_1_1/src/Rectangle.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Rectangle {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         double a, b, field;
 
