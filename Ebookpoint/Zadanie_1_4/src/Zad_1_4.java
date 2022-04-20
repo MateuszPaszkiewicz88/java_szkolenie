8481efb831a5bed4da6526f2c85829f2a9c23096
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 11:00:15 +0200

    Add br.

---
+++ Ebookpoint/Zadanie_1_4/src/Zad_1_4.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Zad_1_4 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double r, result;
+        System.out.println("Program obliczający objętość kuli.");
+    }
+}
