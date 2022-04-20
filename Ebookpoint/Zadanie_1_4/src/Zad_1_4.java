451773d439a8d46ff1e58c2d51d07d6795c9bed4
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 11:07:58 +0200

    add math.pow

--- Ebookpoint/Zadanie_1_4/src/Zad_1_4.java
+++ Ebookpoint/Zadanie_1_4/src/Zad_1_4.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Zad_1_4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         double r, result;
         System.out.println("Program obliczający objętość kuli.");
+        System.out.println("Proszę podać promień kuli: ");
+        r = Double.parseDouble(br.readLine());
+        result = (4/3) * Math.PI * Math.pow(r,3);
     }
 }
