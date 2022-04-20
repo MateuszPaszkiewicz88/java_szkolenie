5bfe617e66663bc0a26ef7c4d0340fc10b56943b
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 10:54:13 +0200

    Result formater - sq of Pi.

---
+++ Ebookpoint/Zadanie_1_3/src/Zadanie_1_3.java
@@ -0,0 +1,6 @@
+public class Zadanie_1_3 {
+    public static void main(String[] args) {
+        System.out.println("Pi do kwadratu z dokładnością do 2ch miejsc po przecinku.");
+        System.out.printf("Pi ^2 = "+ "%2.2f", Math.sqrt(Math.PI));
+    }
+}
