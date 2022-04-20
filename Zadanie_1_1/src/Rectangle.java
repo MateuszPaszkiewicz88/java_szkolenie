962f070f5101e385fb8b0f47dcdc3e66e6bcbe0b
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-20 09:15:13 +0200

    Write code to count rectangle field.

--- Zadanie_1_1/src/Rectangle.java
+++ Zadanie_1_1/src/Rectangle.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
 public class Rectangle {
-    double a,b, field;
 
     public static void main(String[] args) {
+
+        double a, b, field;
+
         Scanner scanner = new Scanner(System.in);
 
-
+        System.out.println("Podaj długość boku a: ");
+        a = scanner.nextDouble();
+        System.out.println("Podaj długość boku b: ");
+        b = scanner.nextDouble();
+        field = a * b;
+        System.out.println("Pole prostokąta wynosi: "+ field +".");
     }
 }
