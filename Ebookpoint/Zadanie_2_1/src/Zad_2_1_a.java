315dee15bc9c6b29c2b92fa2c357153d71524511
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-21 12:21:44 +0200

    2.1a - better solution, no need to put traingle sides in order

---
+++ Ebookpoint/Zadanie_2_1/src/Zad_2_1_a.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Zad_2_1_a {
+    public static void main(String[] args) throws IOException {
+        int a,b,c;
+
+        List<Integer> triangles = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Program weryfikujący, czy trójkąt jest prostokątny.");
+        System.out.println("Podaj długość boku a: ");
+        a = Integer.parseInt(br.readLine());
+        triangles.add(a);
+        System.out.println("Podaj długość boku b: ");
+        b = Integer.parseInt(br.readLine());
+        triangles.add(b);
+        System.out.println("Podaj długość boku b: ");
+        c = Integer.parseInt(br.readLine());
+        triangles.add(c);
+
+        triangles.sort(Comparator.naturalOrder());
+        int side_a = triangles.get(0);
+        int side_b = triangles.get(1);
+        int side_c = triangles.get(2);
+
+        if(side_a * side_a + side_b * side_b == side_c * side_c){
+            System.out.println("Podany trójkąt jest prostokątny.");
+        }else {
+            System.out.println("Trójkąt nie jest prostokątny.");
+        }
+    }
+}
