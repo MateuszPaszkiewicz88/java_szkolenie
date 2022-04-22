b90a358115d9e6d45236cdffd783f274239daa43
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-22 14:48:29 +0200

    Show proper valu after a equals to 0.

--- Ebookpoint/Zadanie_2_3/src/Zad_2_3.java
+++ Ebookpoint/Zadanie_2_3/src/Zad_2_3.java
@@ -5,8 +5,7 @@ import java.io.InputStreamReader;
 
 public class Zad_2_3 {
     public static void main(String[] args) throws IOException {
-        double a, b, c, x, x1, x2, delta = 0;
-        int howManyPower = 0;
+        double a, b, c, x, x1, x2, delta;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Podaj wartość parametru 'a'.");
@@ -17,21 +16,24 @@ public class Zad_2_3 {
         c = Double.parseDouble(br.readLine());
         if (a == 0) {
             System.out.println("Rówanie jest równaniem liniowy.");
-        } else delta = (Math.pow(b, 2) - (4 * a * c));
-        if (delta < 0) howManyPower = 0;
-        if (delta == 0) howManyPower = 1;
-        if (delta > 0) howManyPower = 2;
-        switch (howManyPower) {
-            case 0 -> System.out.println("Brak pieriastków");
-            case 1 -> {
-                x = -b / (2 * a);
-                System.out.println("Rozwiązaniem równania jest x = " + x);
-            }
-            case 2 -> {
-                x1 = (-b - Math.sqrt(delta)) / (2 * a);
-                x2 = (-b + Math.sqrt(delta)) / (2 * a);
-                System.out.println("Wartość x1 = " + x1);
-                System.out.println("Wartość x2 = " + x2);
+        } else {
+            delta = (Math.pow(b, 2) - (4 * a * c));
+            int howManyPower = 0;
+            if (delta < 0) howManyPower = 0;
+            if (delta == 0) howManyPower = 1;
+            if (delta > 0) howManyPower = 2;
+            switch (howManyPower) {
+                case 0 -> System.out.println("Brak pieriastków");
+                case 1 -> {
+                    x = -b / (2 * a);
+                    System.out.println("Rozwiązaniem równania jest x = " + x);
+                }
+                case 2 -> {
+                    x1 = (-b - Math.sqrt(delta)) / (2 * a);
+                    x2 = (-b + Math.sqrt(delta)) / (2 * a);
+                    System.out.println("Wartość x1 = " + x1);
+                    System.out.println("Wartość x2 = " + x2);
+                }
             }
         }
     }
