7a8787f9c58d12dc0ffb1665e04167c13e7c2213
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-22 14:06:19 +0200

    remove delta print

--- Ebookpoint/Zadanie_2_2/src/Zad_2_2.java
+++ Ebookpoint/Zadanie_2_2/src/Zad_2_2.java
@@ -20,7 +20,6 @@ public class Zad_2_2 {
             throw new RuntimeException("Podana wartość musi być liczbą.");
         }
         delta = (b * b) - (4 * a * c);
-        System.out.println(delta);
         if(a==0){
             System.out.println("Jeżeli wartość parametru a wynosi 0, równanie staje się równaniem liniowym.");
         } else if (delta>0) {
