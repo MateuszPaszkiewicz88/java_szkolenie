0e215d91d34654a8a68f185094fef222a30a78f7
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-26 12:29:26 +0200

    Solved 3.3. while loop

---
+++ Ebookpoint/Zadanie_3_3/src/Zad_3_3.java
@@ -0,0 +1,13 @@
+public class Zad_3_3 {
+    public static void main(String[] args) {
+        System.out.println("Obliczanie wartości y = 3x, dla x będącego w przedziale od 0 do 10.");
+        int a =0, y;
+
+        while(a<=10){
+            y = 3 *a;
+            System.out.println("Dla x =  " + a + ", wartość wyrażenia wynosi: " + y + ".");
+            a++;
+        }
+
+    }
+}
