0c1ef26a2c13eaada75acd9b592f5c01ffd16728
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-26 12:21:02 +0200

    Solved 3.1 for loop

---
+++ Ebookpoint/Zadanie_3_1/src/Zad_3_1.java
@@ -0,0 +1,11 @@
+public class Zad_3_1 {
+    public static void main(String[] args) {
+        System.out.println("Obliczanie wartości y = 3x, dla x będącego w przedziale od 0 do 10.");
+        int a,y;
+        for (a=0;a<=10;a++){
+            y = 3*a;
+            System.out.println("Dla x =" + a +", wartość wyrażenia wynosi: "+ y + "." );
+        }
+
+    }
+}
